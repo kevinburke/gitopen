af0afc47f58a0fec541b8483c9ff01dc539f2cac
Kevin Burke <kev@inburke.com>
2016-06-21 09:04:49 -0700

    Better work to support googlesource.com urls
    It's not much, but it lets you open commits for the Go repo in your browser.
    I'll figure out a better solution for the branching later, maybe with objects,
    probably not.

--- gitopen_test.py
+++ gitopen_test.py
@@ -9,3 +9,10 @@ gitopen = imp.load_source('gitopen', os.path.join(directory, 'gitopen'))
 def test_raw_host():
     assert_equal('github.com', gitopen._get_host('github.com-kevinburke'))
     assert_equal('github.com', gitopen._get_host('github.com'))
+
+def test_parse():
+    assert(gitopen._parse_git_remote_url('https://go.googlesource.com/go'))
+    assert(gitopen._parse_git_remote_url('git@github.com-kevinburkeshyp:Shyp/rickover.git'))
+    assert(gitopen._parse_git_remote_url('ssh://git@bitbucket.org/kevinburke/small-dotfiles.git'))
+    assert(gitopen._parse_git_remote_url('https://git.heroku.com/pure-taiga-6832.git'))
+    assert_equal(None, gitopen._parse_git_remote_url('blah'))
